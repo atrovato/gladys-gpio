279ec2f35c84ae4e6b7ebc8a59a6a4945a8619df
Alexandre Trovato <alexandre.trovato@gmail.com>
2018-10-01 20:37:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const watch = require('./lib/watchAll.js');
+
+module.exports = function(sails) {
+    gladys.on('ready', function() {
+        // Read devices every 10 seconds
+        setInterval(watch, 10000);
+    });
+
+    return {
+        setup: sensor
+    };
+};

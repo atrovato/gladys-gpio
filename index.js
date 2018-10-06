60a9ae8d286289af9ef5ef6862a46f38df6f7ddc
Alexandre Trovato <alexandre.trovato@gmail.com>
2018-10-06 22:33:06 +0200

    Logs and some improvment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const setup = require('./lib/install.js');
 
 module.exports = function(sails) {
     gladys.on('ready', function() {
+        console.log('GPIO scanner available');
         // Read devices every 10 seconds
         setInterval(watch, 10000);
     });

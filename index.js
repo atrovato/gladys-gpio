f5d8db0a34da8dc5ccb8f179eaf289d68d108d98
Alexandre Trovato <alexandre.trovato@gmail.com>
2018-10-30 19:00:30 +0100

    Formatting files

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const watch = require('./lib/watchAll.js');
 const setup = require('./lib/install.js');
 
 module.exports = function(sails) {
-    gladys.on('ready', function() {
-        console.log('GPIO scanner available');
-        // Read devices every 10 seconds
-        setInterval(watch, 10000);
-    });
+  gladys.on('ready', function() {
+    console.log('GPIO scanner available');
+    // Read devices every 10 seconds
+    setInterval(watch, 10000);
+  });
 
-    return {
-        setup: setup
-    };
+  return {
+    setup: setup
+  };
 };

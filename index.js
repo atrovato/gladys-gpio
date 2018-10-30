30c9b0c57c796fe3dcee815db5080e79f10eee67
Alexandre Trovato <alexandre.trovato@gmail.com>
2018-10-30 20:04:14 +0100

    Improve logs + rename + change catalog

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const watch = require('./lib/watchAll.js');
-const setup = require('./lib/install.js');
+const setup = require('./lib/setup.js');
 
 module.exports = function(sails) {
   gladys.on('ready', function() {
-    console.log('GPIO scanner available');
     // Read devices every 10 seconds
     setInterval(watch, 10000);
   });
